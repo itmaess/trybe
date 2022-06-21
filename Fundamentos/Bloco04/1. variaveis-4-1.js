253a115952507d8f051b03c95bed238f03543154
itmaess <itmaess@gmail.com>
2022-06-21 18:56:24 -0300

    copia da pasta fundamentos

---
+++ Fundamentos/Bloco04/1. variaveis-4-1.js
@@ -0,0 +1,15 @@
+const myName = "Igor";
+const birthCity = "Aracaju";
+let birthYear = 1994;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Itabaiana";
+
+console.log(birthCity);
---
+++ Fundamentos/Bloco04/3. condicoes-if-else-4-1.js
@@ -0,0 +1,9 @@
+const minhaNota = 80;
+
+if (minhaNota >= 80) {
+    console.log("Parabéns, você foi aprovado(a)!")
+} else if (minhaNota < 80 && minhaNota >=60) {
+    console.log("Você está na nossa lista de espera")
+} else {
+    console.log("Você foi reprovado(a)")
+}
